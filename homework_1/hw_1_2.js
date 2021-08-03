ea4785156e98fd2fa57a4c36361f3f38aa89d796
AnnaZuravleva <ansiv@yandex.ru>
2021-08-03 18:34:21 +0300

    wip: 2nd task add

--- homework_1/hw_1_2.js
+++ homework_1/hw_1_2.js
@@ -1,31 +1,16 @@
-const http = require("http");
+const http = require('http');
+const url = require('url');
 
 http
   .createServer((request, response) => {
-    // response.statusCode = 200;
-    // response.setHeader("Content-Type", "text/html; charset=UTF-8");
-    // response.setHeader("Transfer-Encoding", "chunked");
-
-    // let i = 0;
-    // const timer = setInterval(
-    //   (onEnd) => {
-    //     response.write(response.);
-    //     i += 1;
-    //     if (i > 9) {
-    //       clearInterval(timer);
-    //       onEnd();
-    //     }
-    //   },
-    //   500,
-    //   () => response.end("hello world!")
-    // );
-    console.log(`Request came: ${request.url}`);
-    if (request.url === "/") {
-      sendResponse("index.html", "text/html", response);
+    if (request.url === '/stats') {
+      response.end(`IP: ${request.socket.remoteAddress}, Date: ${new Date().toLocaleString()}`);
+    } else if (request.url === '/users') {
+      response.end('User creation will be available soon');
     } else {
-      sendResponse(request.url, getContentType(request.url), response);
+      response.statusCode = 404;
+      throw new Error('404 Not found');
     }
-    response.end('request');
   })
   .listen(8080, () =>
     console.log("Server is listening at http://localhost:8080")
